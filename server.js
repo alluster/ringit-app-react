5ab2d0b40329362ddf66617e4a597ad0602542ae
Aleksanteri Heliövaara <aleksanteriheliovaara@AleksanterisMBP.home>
2020-05-27 13:20:52 +0300

    first commit

--- server.js
+++ server.js
@@ -11,13 +11,13 @@ app.use(express.static(path.join(__dirname, 'client/build')));
 if(process.env.NODE_ENV === 'production') {  
 	app.use(express.static(path.join(__dirname, 'client/build')));  
 	app.get('*', (req, res) => { 
-		res.sendfile(path.join(__dirname = 'client/build/index.html'));  
+		res.sendFile(path.join(__dirname = 'client/build/index.html'));  
 	})
 }
 
 
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
+// app.use(bodyParser.json());
+// app.use(bodyParser.urlencoded({ extended: true }));
 
 
 
