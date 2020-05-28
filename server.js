143601c0315f11afe7ef51384aeb79b8cd0451be
Aleksanteri Heliövaara <aleksanteriheliovaara@AleksanterisMBP.home>
2020-05-28 15:28:58 +0300

    hooks functionalities fixed

--- server.js
+++ server.js
@@ -7,11 +7,11 @@ const SQL = require('sql-template-strings')
 const sslRedirect = require('heroku-ssl-redirect');
 const bodyParser = require('body-parser')
 const path = require('path')
-app.use(express.static(path.join(__dirname, 'build')));
+app.use(express.static(path.join(__dirname, '../client/build')));
 
 
 app.get('/*', (req, res) => {
-  res.sendFile(path.join(__dirname, 'build', 'index.html'));
+  res.sendFile(path.join(__dirname, '../client/build/index.html'));
 });
 app.use(sslRedirect());
 
