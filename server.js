f70ef4bcc7721d4ccf0d26e2e24755b338122600
Aleksanteri Heliövaara <aleksanteriheliovaara@AleksanterisMBP.home>
2020-05-29 11:27:03 +0300

    hooks functionalities fixed

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ const path = require('path')
 const publicPath = path.join(__dirname, '..', 'public');
 app.use(express.static(publicPath));
 app.get('*', (req, res) => {
-	res.sendFile(path.join(publicPath, 'index.html'));
+	res.sendFile(path.join(publicPath, 'public/index.html'));
  });
 
 app.use(bodyParser.json())
